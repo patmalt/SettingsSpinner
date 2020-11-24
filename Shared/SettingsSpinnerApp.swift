a0dd3aa19d7b9535443bbaed01b6c0e7feb58b46
Patrick Maltagliati <patmalt@gmail.com>
2020-11-24 09:27:43 -0600

    Initial Commit

---
+++ Shared/SettingsSpinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsSpinnerApp.swift
+//  Shared
+//
+//  Created by Patrick Maltagliati on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct SettingsSpinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
