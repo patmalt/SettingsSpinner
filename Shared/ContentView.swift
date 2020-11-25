9e4cc93cb3dbc43d8f4d948384d47a515dc0f05e
Patrick Maltagliati <patmalt@gmail.com>
2020-11-25 09:44:27 -0600

    Updates

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -14,7 +14,10 @@ struct ContentView: View {
         ZStack {
             RoundedRectangle(cornerRadius: 16).fill(Color.gray).frame(width: 200, height: 200)
             Circle().fill(Color.black).frame(width: 180, height: 180)
-            SpikeCircle().frame(width: 160, height: 160).rotationEffect(Angle.init(degrees: isRotated ? 360 : 0)).animation(Animation.linear.repeatForever(autoreverses: false))
+            SpikeCircle().frame(width: 160, height: 160).rotationEffect(Angle.init(degrees: isRotated ? 360 : 0)).animation(Animation.linear(duration: 200).repeatForever(autoreverses: false))
+            SpikeCircle().frame(width: 120, height: 120).rotationEffect(Angle.init(degrees: isRotated ? 0 : 360)).animation(Animation.linear(duration: 100).repeatForever(autoreverses: false))
+            SpikeCircle().frame(width: 80, height: 80).rotationEffect(Angle.init(degrees: isRotated ? 360 : 0)).animation(Animation.linear(duration: 50).repeatForever(autoreverses: false))
+            SpikeCircle().frame(width: 40, height: 40).rotationEffect(Angle.init(degrees: isRotated ? 0 : 360)).animation(Animation.linear(duration: 25).repeatForever(autoreverses: false))
         }
         .onAppear {
             isRotated.toggle()
@@ -39,6 +42,7 @@ class SpikeCircleView: UIView {
         super.init(frame: frame)
         replicatorInstanceLayer.anchorPoint = CGPoint(x: 0.5, y: 1)
         replicatorInstanceLayer.strokeColor = UIColor.darkGray.cgColor
+        replicatorInstanceLayer.lineWidth = 0.5
         replicator.addSublayer(replicatorInstanceLayer)
         layer.addSublayer(replicator)
     }
@@ -53,12 +57,9 @@ class SpikeCircleView: UIView {
         let radius = diameter / 2
         let circumference = CGFloat.pi * diameter
         let numberOfImages = Int(circumference / spikeSize)
-        var perspectiveTransform = CATransform3DIdentity
-        perspectiveTransform.m34 = 1 / -radius
         let rect = CGRect(x: radius - spikeSize / 2, y: -spikeSize / 2, width: spikeSize, height: spikeSize)
         replicatorInstanceLayer.frame = rect
         replicatorInstanceLayer.path = CGPath.spike(in: rect)
-        replicatorInstanceLayer.transform = perspectiveTransform
         replicator.instanceCount = numberOfImages
         replicator.instanceTransform = CATransform3DRotate(CATransform3DIdentity, .pi / (CGFloat(numberOfImages) / 2), 0, 0, 1)
         replicator.frame = bounds
@@ -68,14 +69,12 @@ class SpikeCircleView: UIView {
 extension CGPath {
     static func spike(in rect: CGRect) -> CGPath {
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: 2, y: 0))
-        path.addLine(to: CGPoint(x: 0, y: rect.height * 4))
-        path.move(to: CGPoint(x: 2, y: 0))
-        path.addCurve(
-            to: CGPoint(x: 4, y: rect.height * 4),
-            controlPoint1: CGPoint(x: 4, y: rect.height * 4),
-            controlPoint2: CGPoint(x: 4, y: rect.height * 4)
-        )
+
+        path.move(to: CGPoint(x: 3, y: 8))
+        path.addCurve(to: CGPoint(x: 4, y: 1), controlPoint1: CGPoint(x: 3, y: 8), controlPoint2: CGPoint(x: 3, y: 1))
+        path.addCurve(to: CGPoint(x: 5, y: 8), controlPoint1: CGPoint(x: 5, y: 1), controlPoint2: CGPoint(x: 5, y: 8))
+        path.close()
+
         return path.cgPath
     }
 }
